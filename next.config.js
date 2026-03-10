a5a039bb953f295b7435ab9af95f338c0a60ae0d
John Crawford III <jcrawf09@villanova.edu>
2026-03-10 12:57:16 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,1 +0,0 @@
-const sh=[{key:'X-Frame-Options',value:'SAMEORIGIN'},{key:'X-Content-Type-Options',value:'nosniff'},{key:'Referrer-Policy',value:'strict-origin-when-cross-origin'},{key:'Permissions-Policy',value:'camera=(), microphone=(), geolocation=()'}];const nextConfig={async headers(){return[{source:'/(.*)',headers:sh}]}};module.exports=nextConfig;
